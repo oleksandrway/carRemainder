22fff48d391cbdc74f75dcc2fd6a73fba54d20ac
Oleksandr Kryvoruchko <oleksandrkryvoruchko2000@gmail.com>
2023-05-15 19:24:56 +0300

    vue ts template

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+import { useCounterStore } from './stores/counter'
+
+const counter = reactive(useCounterStore())
+
+</script>
+
+<template>
+  <TheHeader />
+  <RouterView />
+  <div class="flex justify-center items-center gap-20px py-30px text-20px">
+    {{ counter.count }}
+    <button class="bg-gray-400 p-5px" @click="counter.increment">
+      increment
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const greeting = 'hello there'
+</script>
+
+<template>
+  Home
+  {{ greeting }}
+</template>
